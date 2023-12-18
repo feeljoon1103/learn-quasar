d49866aea589aa4382796115b74e16345842762b
feeljoon1103 <feeljoon1103@gmail.com>
2023-12-18 17:03:25 +0900

    [20231218] Quasar 인스타르렘 header

--- src/pages/FlexGrid2.vue
+++ src/pages/FlexGrid2.vue
@@ -1,10 +1,35 @@
 <template>
-  <q-page class="q-pa-xl">
-    <section class="q-mb-xl">
-      <div class="text-h4">Flex Grid 2</div>
-      <q-separator class="q-my-md"/>
-
+  <q-page padding>
+    <section class="row q-col-gutter-x-xl q-py-lg">
+      <div class="col-auto q-py-md">
+        <q-avatar size="150px">
+          <q-img src="/success.png"/>
+        </q-avatar>
+      </div>
+      <div class="col-grow column justify-between">
+        <div class="row justify-between items-center">
+          <span class="text-h6 text-weight-bold">jun-krd</span>
+          <q-space/>
+          <div class="q-gutter-x-sm">
+            <q-btn outline color="secondary" label="프로필 편집"></q-btn>
+            <q-btn outline color="secondary" label="프로필 편집"></q-btn>
+            <q-btn outline color="secondary" label="프로필 편집"></q-btn>
+          </div>
+          <q-btn flat color="secondary" icon="more_horiz"></q-btn>
+        </div>
+        <div class="q-gutter-x-lg">
+          <span>게시글 238</span>
+          <span>팔로워 2,032</span>
+          <span>팔로우 1,432</span>
+        </div>
+        <div>
+          <div>개발자</div>
+          <div>#INFP</div>
+        </div>
+      </div>
     </section>
+    <section class="bg-yellow"></section>
+    <section class="bg-green"></section>
   </q-page>
 </template>
 
@@ -13,5 +38,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.q-page {
+  background-color: #fafafa;
+}
 </style>
