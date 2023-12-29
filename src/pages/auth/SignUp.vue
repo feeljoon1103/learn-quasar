241b9f720761fc7c50c4a9f443e8dfd59c4dfc90
feeljoon1103 <feeljoon1103@gmail.com>
2023-12-29 14:36:35 +0900

    [20231229] Quasar 로그인페이지 0.2v

--- src/pages/auth/SignUp.vue
+++ src/pages/auth/SignUp.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="text-h6">SignUp</div>
+    <q-card flat :style="{minWidth: '400px'}">
+      <q-card-section>
+        <div class="text-h6 text-weight-bold">회원가입</div>
+      </q-card-section>
+      <q-card-section>
+        <q-form>
+          <div class="q-gutter-y-lg">
+            <q-input filled label="이메일" hint="Email을 입력해주세요."></q-input>
+            <q-input filled type="password" label="비밀번호" hint="영문 대/소문자 포함 8자 이상"></q-input>
+            <q-input filled type="password" label="비밀번호 확인" hint="영문 대/소문자 포함 8자 이상"></q-input>
+            <q-input input-class="text-right" filled label="나이" prefix="만 나이" suffix="세"></q-input>
+            <q-input filled v-model="Card" mask="#### - #### - #### - ####" fill-mask="#" unmasked-value label="Card"></q-input>
+          </div>
+          <q-btn class="full-width q-mt-lg" unelevated color="primary" size="lg" label="가입하기"/>
+          <q-btn class="full-width q-mt-md" flag label="로그인" to="/auth/sign-in"/>
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
