730e21b5b5e87971bb2b9d7606e434f4a95e6825
feeljoon1103 <feeljoon1103@gmail.com>
2024-01-05 14:51:35 +0900

    [20240105] Quasar QForm Handling 0.4v

--- src/pages/FormHandling.vue
+++ src/pages/FormHandling.vue
@@ -2,12 +2,25 @@
   <q-page class="q-pa-xl">
     <section class="q-md-xl">
       <div class="text-h4">QForm Component</div>
-      <q-separator class="q-my-md"/>
-      <q-form class="q-gutter-y-md q-mt-lg">
-        <q-input outlined v-model="form.title" label="제목" :rules="[(val) => !!val || '필수 항목입니다.']"/>
-        <q-input outlined v-model="form.content" label="내용" type="textarea" hint="50자 이내로 입력해주세요." counter/>
+      <q-separator class="q-my-md" />
+      <!-- greedy => 전체 유효성 검사, autofocus => 첫 화면시 포커스 지정 -->
+      <q-form ref="myForm" class="q-gutter-y-md q-mt-lg" autofocus greedy @submit="onSubmit" @reset="onReset">
+        <q-input outlined v-model="form.title" label="제목" :rules="[(val) => !!val || '필수 항목입니다.']" />
+
+        <q-input outlined v-model="form.content" label="내용" type="textarea" counter
+                  :rules="[
+                    val => !!val || '필수 항목입니다.',
+                    val => val.length <= 50 || '최대 50자 이내로 입력하세요.',
+                  ]"
+        />
         <!-- emit-value는 value값만 추출 -->
-        <q-select outlined v-model="form.tags" :options="tagOptions" label="태그" emit-value multiple hint="최대 2개 선택 가능합니다."/>
+        <q-select outlined v-model="form.tags" :options="tagOptions" label="태그" emit-value multiple
+                  hint="최대 2개 선택 가능합니다."
+                  :rules="[
+                  val => !!val || '필수 항목입니다.',
+                  val => val.length <= 2 || '최대 2개 까지 선택 가능합니다.',
+        ]"
+        />
 
         <q-input outlined v-model="form.date" mask="date" :rules="['date']">
           <template v-slot:append>
@@ -30,9 +43,11 @@
           true-value="Agreed"
         />
 
-        <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
-          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"/>
+        <div class="q-gutter-x-sm">
+          <q-btn label="validate" color="secondary" @click="validate"/>
+          <q-btn label="resetValidation" color="warning" @click="reset"/>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
         </div>
 
       </q-form>
@@ -44,21 +59,53 @@
 <script setup>
 import { ref } from "vue";
 
+const myForm = ref(null);
 const form = ref({
   title: "",
   content: "",
   tags: [],
   date: "2022/01/01",
-  accept: false,
+  accept: false
 });
 //const tagOptions = ref(["Google", "Facebook", "Twitter", "Apple", "Oracle"]);
 const tagOptions = ref([
-  {label: "구글", value: "Google"},
-  {label: "페이스북", value: "Facebook"},
-  {label: "트위터", value: "Twitter"},
-  {label: "애플", value: "Apple"},
-  {label: "오라클", value: "Oracle"},
+  { label: "구글", value: "Google" },
+  { label: "페이스북", value: "Facebook" },
+  { label: "트위터", value: "Twitter" },
+  { label: "애플", value: "Apple" },
+  { label: "오라클", value: "Oracle" }
 ]);
+
+const validate = () => {
+  myForm.value.validate().then((success) => {
+    if (success) {
+      alert("성공입니다!");
+    } else {
+      alert("실패입니다!");
+    }
+  })
+}
+
+const reset = () => {
+  myForm.value.resetValidation();
+}
+
+const onSubmit = () => {
+  if (form.value.accept !== true) {
+    alert("동의 해주세요.");
+  } else {
+    alert("성공!");
+  }
+}
+
+const onReset = () => {
+  form.value.title = "";
+  form.value.content = "";
+  form.value.tags = [];
+  form.value.date = "2022/01/01";
+  form.value.accept = false;
+}
+
 </script>
 
 <style lang="scss" scoped>
