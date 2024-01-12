88fb70c9e5fc3052d212df5b5c967f42ec4fd4bb
feeljoon1103 <feeljoon1103@gmail.com>
2024-01-12 17:23:54 +0900

    [20240112] Quasar 다국어 적용 0.1v

--- quasar.config.js
+++ quasar.config.js
@@ -85,6 +85,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
+      lang: "ko-KR",
       cssAddon: true,
       config: {
         screen: {
